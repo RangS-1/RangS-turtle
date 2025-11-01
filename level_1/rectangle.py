179a27377846a2f94e267a61fdb642ceffd549b4
RangS <rangga19sj@gmail.com>
2025-11-01 16:53:27 +0700

    Create rectangle.py

---
+++ level_1/rectangle.py
@@ -0,0 +1,20 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.backward(200)
+t.forward(400)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(400)
+t.right(90)
+t.forward(400)
+t.right(90)
+t.forward(400)
+t.right(90)
+t.forward(180)
+t.right(90)
+t.forward(400)
+
+turtle.done()
