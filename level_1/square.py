615ced03926106920ab0812a04c97799317e9cf0
RangS <rangga19sj@gmail.com>
2025-11-01 16:47:47 +0700

    Square

---
+++ level_1/square.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.backward(200)
+
+for i in range(4):
+    turtle.forward(200)
+    turtle.right(90)
+
+turtle.forward(200)
+
+for i in range(4):
+    turtle.forward(200)
+    turtle.right(90)
+
+for i in range(4):
+    turtle.forward(200)
+    turtle.left(90)
+
+for i in range(4):
+    turtle.backward(200)
+    turtle.right(90)
+
+turtle.done()
